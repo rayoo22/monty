836b3561cae4cc743a6c53017357f4d99193ec71
rayoo22 <ryankibet14@gmail.com>
2023-12-15 07:55:03 -0800

    monty interpreter

--- monty_func.c
+++ monty_func.c
@@ -32,7 +32,7 @@ void read_file(char *filename, stack_t **stack)
 		s = get_op_func(line);
 		if (s == NULL)
 		{
-			fprintf(stderr, "L%d: unknown instruction %d\n", line_count);
+			fprintf(stderr, "L%d: unknown instruction\n", line_count);
 			exit(EXIT_FAILURE);
 		}
 		s(stack, line_count);
